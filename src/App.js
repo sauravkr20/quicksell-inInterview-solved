d1b43770062069b11a3f7ace57645f5354074220
sauravkr20 <sauravkr20@iitk.ac.in>
2023-12-04 07:32:53 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [counter, setCounter] = useState(0);
+
+	const handleDec = () => {
+		if (counter === 0) {
+			return;
+		} else {
+			setCounter((prevVale) => setCounter(parseInt(prevVale) - 1));
+		}
+	};
+
+	const handleInc = () => {
+		if (counter === 1000) {
+			return;
+		} else {
+			setCounter((prevVale) => setCounter(parseInt(prevVale) + 1));
+		}
+	};
+
+	return (
+		<div className="App">
+			<div className="input-container">
+				<button onClick={handleDec} className="dec-button">
+					-
+				</button>
+				<input
+					value={counter}
+					onChange={(e) => setCounter(e.target.value)}
+					className="input-box"
+				></input>
+				<button onClick={handleInc} className="inc-button">
+					+
+				</button>
+			</div>
+		</div>
+	);
 }
 
 export default App;
